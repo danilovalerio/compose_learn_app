c1c30ae29295078b061d94b9ff28aa4dbd0fe494
danilovalerio <danilo_vs@hotmail.com>
2023-07-10 18:29:55 -0300

    definimos novas cores

--- app/src/main/java/com/example/composelearnapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/composelearnapp/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.composelearnapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//Cores padrão que estamos utilizando até o momento
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Novas cores
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
