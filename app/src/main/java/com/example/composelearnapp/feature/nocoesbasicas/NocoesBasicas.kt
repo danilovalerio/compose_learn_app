d3ac0f02f62ed28898e8f615394bb12b6a27b78e
danilovalerio <danilo_vs@hotmail.com>
2023-07-10 17:29:44 -0300

    Tela para Integração

--- app/src/main/java/com/example/composelearnapp/feature/nocoesbasicas/NocoesBasicas.kt
+++ app/src/main/java/com/example/composelearnapp/feature/nocoesbasicas/NocoesBasicas.kt
@@ -3,18 +3,23 @@ package com.example.composelearnapp.feature.nocoesbasicas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,14 +59,18 @@ private fun Saudacoes(name: String) {
     val expanded = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
-    Surface(color = MaterialTheme.colorScheme.primary,
-    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
             // Column, Row e Box são os 3 elementos básicos de layout no compose.
-            Column(modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
@@ -79,5 +88,32 @@ private fun Saudacoes(name: String) {
 private fun DefaultPreview() {
     ComposeLearnAppTheme {
         MyApp()
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Codelab Basics Compose")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    ComposeLearnAppTheme {
+        OnboardingScreen()
     }
 }
