c8c6c6d3571bce46a3b45e5c2938a53d2fc3c29b
danilovalerio <danilo_vs@hotmail.com>
2023-07-10 18:59:36 -0300

    remove bibliotecas sem importacoes

--- app/src/main/java/com/example/composelearnapp/feature/nocoesbasicas/NocoesBasicas.kt
+++ app/src/main/java/com/example/composelearnapp/feature/nocoesbasicas/NocoesBasicas.kt
@@ -19,7 +19,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
