eeef692fb50aad53e89eacda7568117a351c4c44
danilovalerio <danilo_vs@hotmail.com>
2023-07-10 17:05:20 -0300

    Add Linha e Botao

--- app/src/main/java/com/example/composelearnapp/feature/nocoesbasicas/NocoesBasicas.kt
+++ app/src/main/java/com/example/composelearnapp/feature/nocoesbasicas/NocoesBasicas.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -48,11 +50,22 @@ fun MyApp(
 private fun Saudacoes(name: String) {
     Surface(color = MaterialTheme.colorScheme.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        // Column, Row e Box são os 3 elementos básicos de layout no compose.
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello, ")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            // Column, Row e Box são os 3 elementos básicos de layout no compose.
+            Column(modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(24.dp)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Show more")
+            }
         }
+
     }
 }
 
